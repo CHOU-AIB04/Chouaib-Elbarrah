76f4b172e47153820d8e12791623c25435be3671
CHOU-AIB04 <142614891+CHOU-AIB04@users.noreply.github.com>
2024-02-20 21:12:54 +0100

    fixinggg

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     }
   return (
     <nav className='fixed z-40 bottom-0 phone:top-1/2 phone:-translate-y-1/2  right-0 flex flex-row phone:flex-col color w-[80%] phone:w-12 mr-12 phone:mr-7 items-center h-12 phone:h-72 justify-around rounded-2xl'>
-        <NavLink to="/Chouaib-Elbarrah/" style={({isActive})=> isActive ? style : {color : "white"}}><i className="bi bi-house-door-fill text-[19px]"></i></NavLink>
+        <NavLink to="/Chouaib-Elbarrah/" end style={({isActive})=> isActive ? style : {color : "white"}}><i className="bi bi-house-door-fill text-[19px]"></i></NavLink>
         <NavLink to="/Chouaib-Elbarrah/about" style={({isActive})=> isActive ? style : {color : "white"}}><i className="bi bi-person-fill text-[19px]"></i></NavLink>
         <NavLink to="/Chouaib-Elbarrah/Skils" style={({isActive})=> isActive ? style : {color : "white"}}><i className="bi bi-calendar2 text-[19px]"></i></NavLink>
         <NavLink to="/Chouaib-Elbarrah/Work" style={({isActive})=> isActive ? style : {color : "white"}}><i className="bi bi-archive-fill text-[19px]"></i></NavLink>
