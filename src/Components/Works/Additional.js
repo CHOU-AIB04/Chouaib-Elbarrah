02796df277f1b3055933f860c9ddcd564761ae94
CHOU-AIB04 <chouaibalx@gmail.com>
2024-08-26 19:57:25 +0100

    adding some full-stack project

---
+++ src/Components/Works/Additional.js
@@ -0,0 +1,78 @@
+// project 1
+import p1_pic1 from '../../picture/project1/pic1.png'
+import p1_pic2 from '../../picture/project1/pic2.png'
+import p1_pic3 from '../../picture/project1/pic3.png'
+import p1_pic4 from '../../picture/project1/pic4.png'
+import p1_pic5 from '../../picture/project1/pic5.png'
+// project 2
+import p2_pic1 from '../../picture/project2/pic1.png'
+import p2_pic2 from '../../picture/project2/pic2.png'
+import p2_pic3 from '../../picture/project2/pic3.png'
+import p2_pic4 from '../../picture/project2/pic4.png'
+import p2_pic5 from '../../picture/project2/pic5.png'
+// project 3
+import p3_pic1 from '../../picture/project3/pic1.png'
+import p3_pic2 from '../../picture/project3/pic2.png'
+import p3_pic3 from '../../picture/project3/pic3.png'
+import p3_pic4 from '../../picture/project3/pic4.png'
+import p3_pic5 from '../../picture/project3/pic5.png' 
+// project 4
+import p4_pic1 from '../../picture/project4/pic1.png'
+import p4_pic2 from '../../picture/project4/pic2.png'
+import p4_pic3 from '../../picture/project4/pic3.png'
+import p4_pic4 from '../../picture/project4/pic4.png'
+import p4_pic5 from '../../picture/project4/pic5.png' 
+// project 5
+import p5_pic1 from '../../picture/project5/pic1.png'
+import p5_pic2 from '../../picture/project5/pic2.png'
+import p5_pic3 from '../../picture/project5/pic3.png'
+import p5_pic4 from '../../picture/project5/pic4.png'
+import p5_pic5 from '../../picture/project5/pic5.png' 
+
+export default [
+    {
+        id : 1,
+        project_name : "Cosmetic Project",
+        pic1 : p1_pic1,
+        pic2 : p1_pic2,
+        pic3 : p1_pic3,
+        pic4 : p1_pic4,
+        pic5 : p1_pic5
+    },
+    {
+        id : 2,
+        project_name : "Estate Project",
+        pic1 : p2_pic1,
+        pic2 : p2_pic2,
+        pic3 : p2_pic3,
+        pic4 : p2_pic4,
+        pic5 : p2_pic5
+    },
+    {
+        id : 3,
+        project_name : "Keroumi Dashboard",
+        pic1 : p3_pic1,
+        pic2 : p3_pic2,
+        pic3 : p3_pic3,
+        pic4 : p3_pic4,
+        pic5 : p3_pic5
+    },
+    {
+        id : 4,
+        project_name : "Cycle Project",
+        pic1 : p4_pic1,
+        pic2 : p4_pic2,
+        pic3 : p4_pic3,
+        pic4 : p4_pic4,
+        pic5 : p4_pic5
+    },
+    {
+        id : 5,
+        project_name : "Diamand Project",
+        pic1 : p5_pic1,
+        pic2 : p5_pic2,
+        pic3 : p5_pic3,
+        pic4 : p5_pic4,
+        pic5 : p5_pic5
+    }
+]
--- src/Components/Works/Work.jsx
+++ src/Components/Works/Work.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import Data from './Data'
+import Additional from './Additional'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 const Work = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <section className='w-[80%] h-auto relative top-36 left-1/2 -translate-x-1/2 flex flex-col items-center gap-4'>
+    <>
+     <section className='w-[80%] h-auto relative top-36 left-1/2 -translate-x-1/2 flex flex-col items-center gap-4'>
       <h1 className='text-[30px] text-white font-bold'>MY <span className='text-red-700'>WORKS.</span></h1>
       <section className='h-[90%] w-full grid grid-cols-1 gap-4 md:grid-cols-2 tablet:grid-cols-3'>
         {
@@ -17,6 +29,42 @@ const Work = () => {
         }
       </section>
     </section>
+    <section className='w-[90%] md:w-[80%] mb-20  h-96  mt-20  relative top-36 left-1/2 -translate-x-1/2 flex flex-col items-center gap-4'>
+      <h1 className='text-[20px] md:text-[30px] text-white font-bold'>Additional <span className='text-red-700'>Full-Stack Projects.</span></h1>
+      <section className='h-[90%] w-full grid grid-cols-1 justify-items-center gap-4 md:grid-cols-2 tablet:grid-cols-2'>
+        {
+          Additional.map((el)=>{
+            return(
+              <>
+               <nav className='w-full md:w-[90%] h-72  flex flex-col justify-between'>
+                  <Slider {...settings} className='w-full h-[80%]'>
+                    <div className=' h-64 overflow-hidden rounded-md sh'>
+                      <img src={el.pic1} alt="pic" className='w-full h-full object-cover'/>
+                    </div>
+                    <div className=' h-64 overflow-hidden rounded-md sh'>
+                      <img src={el.pic2} alt="pic" className='w-full h-full object-cover'/>
+                    </div>
+                    <div className=' h-64 overflow-hidden rounded-md sh'>
+                      <img src={el.pic3} alt="pic" className='w-full h-full object-cover'/>
+                    </div>
+                    <div className=' h-64 overflow-hidden rounded-md sh'>
+                      <img src={el.pic4} alt="pic" className='w-full h-full object-cover'/>
+                    </div>
+                    <div className=' h-64 overflow-hidden rounded-md sh'>
+                      <img src={el.pic5} alt="pic" className='w-full h-full object-cover'/>
+                    </div>
+                  </Slider>
+                  <h1 className='text-white uppercase text-center'>{el.project_name}</h1>
+               </nav>
+              </>
+            )
+          })
+        }
+     
+      </section>
+    </section>
+    </>
+   
   )
 }
 
